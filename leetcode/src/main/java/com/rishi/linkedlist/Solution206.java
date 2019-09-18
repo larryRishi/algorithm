69a66afe895d4596bb21abf2f833d71e5706a1ea
rishi <liuhx361@163.com>
2019-09-18 18:59:37 +0800

    快慢指针

--- leetcode/src/main/java/com/rishi/linkedlist/Solution206.java
+++ leetcode/src/main/java/com/rishi/linkedlist/Solution206.java
@@ -12,6 +12,7 @@ public class Solution206 {
     /**
      * 迭代法
      * 暴力破解，会创建新的对象，不是真正的反转
+     *
      * @param head
      * @return
      */
@@ -26,6 +27,12 @@ public class Solution206 {
     }
 
 
+    /**
+     * 迭代法
+     *
+     * @param head
+     * @return
+     */
     static ListNode reverseList2(ListNode head) {
         ListNode newHead = head;
         if (head != null) {
@@ -39,6 +46,54 @@ public class Solution206 {
         return newHead;
     }
 
+    /**
+     * 迭代法
+     *
+     * @param head
+     * @return
+     */
+    static ListNode reverseList3(ListNode head) {
+        ListNode newHead = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode nextNode = cur.next;
+            cur.next = newHead;
+            newHead = cur;
+            cur = nextNode;
+        }
+        return newHead;
+    }
+
+    /**
+     * 递归法
+     *
+     * @param head
+     * @return
+     */
+    static ListNode reverseList4(ListNode head) {
+        return reverse(head);
+    }
+
+    static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = null;
+        ListNode reverse = reverse(next);
+        append(head, reverse);
+        return reverse;
+    }
+
+    static void append(ListNode head, ListNode reverse) {
+        for (ListNode node = reverse; ; node = node.next) {
+            if (node.next == null) {
+                node.next = head;
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
@@ -46,10 +101,12 @@ public class Solution206 {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         head.printList();
-        System.out.println("reverseList:");
-        reverseList(head).printList();
-        System.out.println("reverseList2:");
-        reverseList2(head).printList();
+//        System.out.println("reverseList:");
+//        reverseList(head).printList();
+//        System.out.println("reverseList2:");
+//        reverseList2(head).printList();
+//        System.out.println("reverseList3:");
+        reverseList4(head).printList();
     }
 
 }
