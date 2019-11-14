f858b4cd291c6b5403676263c4056b19141533bd
rishi <liuhx361@163.com>
2019-11-14 16:42:52 +0800

    62

--- leetcode/src/main/java/com/rishi/backtrack/Solution62.java
+++ leetcode/src/main/java/com/rishi/backtrack/Solution62.java
@@ -8,7 +8,7 @@ package com.rishi.backtrack;
 public class Solution62 {
 
     public static void main(String[] args) {
-        int paths = new Solution62().uniquePaths(7, 3);
+        int paths = new Solution62().uniquePaths2(7, 3);
         System.out.println(paths);
     }
 
@@ -27,6 +27,33 @@ public class Solution62 {
 
         mem[m][n] = mem[m][n] != 0 ? mem[m][n] : div(m - 1, n, mem) + div(m, n - 1, mem);
         return mem[m][n];
+    }
+
+
+    /**
+     * 使用动态规划来实现
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths2(int m, int n) {
+        int[][] mem = new int[m + 1][n + 1];
+        if (m == 1 && n == 1) {
+            return 1;
+        }
+        for (int i = 1; i < m + 1; i++) {
+            mem[i][n] = 1;
+        }
+        for (int j = 1; j < n + 1; j++) {
+            mem[m][j] = 1;
+        }
+
+        for (int i = m - 1; i > 0; i--) {
+            for (int j = n - 1; j > 0; j--) {
+                mem[i][j] = mem[i + 1][j] + mem[i][j + 1];
+            }
+        }
+        return mem[1][1];
     }
 
 }
